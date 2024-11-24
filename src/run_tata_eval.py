e3c7133048e442c7cf99e64d5a82b054b993fe24
Erick Galinkin <erick.galinkin@gmail.com>
2024-11-24 15:51:37 -0500

    update type agnostic training and eval scripts

--- src/run_tata_eval.py
+++ src/run_tata_eval.py
@@ -1,6 +1,8 @@
 import json
 import copy
 from multiprocessing import Pool
+from argparse import ArgumentParser
+from pathlib import Path
 
 import numpy as np
 import torch
@@ -17,24 +19,14 @@ from multiagent_yt_run import PPO, HierarchicalPPO
 import warnings
 warnings.filterwarnings("ignore")
 
-TRIALS = 1000
-TIMESTEPS = 1000
+TRIALS = 500
+TIMESTEPS = 2 * TRIALS
+RESULTS_PATH = Path("./tata_results/")
 
-gdb = GameModeDB()
-ndb = NetworkDB()
-GAME_MODE = gdb.get("d2c92fc4-119d-42e8-83ab-45be03c64d49")
-NETWORK = ndb.get("3b921390-cd7b-41c5-8120-5e9ac587d2f2")
-
-network_interface = NetworkInterface(GAME_MODE, NETWORK)
-# Create dummy env to establish env parameters
-RED = AdaptiveRed(network_interface)
-BLUE = BlueInterface(network_interface)
-ENV = MultiAgentEnv(RED, BLUE, network_interface, print_metrics=False, print_per_ts_data=False)
-
-blue_state_dim = ENV.observation_space.shape[0]
-blue_action_dim = ENV.action_space.n
-red_state_dim = ENV.observation_space.shape[0]
-red_action_dim = len(RED.action_dict)
+parser = ArgumentParser()
+parser.add_argument("--game", type=str, help="Game ID", default="d2c92fc4-119d-42e8-83ab-45be03c64d49")
+parser.add_argument("--setting", type=str, help="Game setting", choices=["standard", "scaled", "tiny", "zeroscale", "zerotiny"])
+parser.add_argument("--output", type=str, help="Output filename", required=True)
 
 
 # Evaluation function
@@ -68,17 +60,57 @@ def evaluate(env, red_agent, blue_agent, red_type, name):
 
 if __name__ == "__main__":
     mp.set_start_method('spawn')
-    # 1k training runs; k_epochs=5, lr_actor=0.0003, lr_critic=0.0005
-#    multi_type = "48a25525-5dfd-4333-a3c1-6585ac843fb7"
-#    hierarchical = "92238a08-4f95-4317-b8b2-6b6c200dc458"
-#    ransomware = "7f2699af-bdc1-49a2-ad98-eefc158cb3b6"
-#    apt = "029841f2-d7e9-4b69-a1c0-c2ccfccf6f99"
+    args = parser.parse_args()
+    
+    output_path = f"{RESULTS_PATH.joinpath(args.output)}.json"
 
-# 3k training runs; k_epochs=5, lr_actor=0.0003, lr_critic=0.0005
-    multi_type = "5e216b55-2e81-4179-a6b3-f24d73e1cf3f"
-    hierarchical = "618a4f19-65bc-4555-876c-1e0620856a95"
-    ransomware = "6ec6fa23-1224-43f9-8a9f-5faa78c77b5e"
-    apt = "ef631654-1b31-4959-9127-17cf0b437ebf"
+    gdb = GameModeDB()
+    ndb = NetworkDB()
+    GAME_MODE = gdb.get(args.game)
+    NETWORK = ndb.get("3b921390-cd7b-41c5-8120-5e9ac587d2f2")
+
+    network_interface = NetworkInterface(GAME_MODE, NETWORK)
+    # Create dummy env to establish env parameters
+    RED = AdaptiveRed(network_interface)
+    BLUE = BlueInterface(network_interface)
+    ENV = MultiAgentEnv(RED, BLUE, network_interface, print_metrics=False, print_per_ts_data=False)
+
+    blue_state_dim = ENV.observation_space.shape[0]
+    blue_action_dim = ENV.action_space.n
+    red_state_dim = ENV.observation_space.shape[0]
+    red_action_dim = len(RED.action_dict)
+    
+    if args.setting.lower() == "scaled":
+    # Scaled
+        multi_type = "81450b82-847a-4733-967e-842e0d96709f"
+        hierarchical = "379b4b51-c438-4165-96a1-2fb339db24f1"
+        ransomware = "bbfeecd7-3a88-4fa8-82ad-4ed39d70aba0"
+        apt = "47c3ae21-6d3f-48be-bbe7-aab51a2f05a5"
+    
+    elif args.setting.lower() == "tiny":
+    # Tiny
+        multi_type = "96744406-1d3e-427c-9a9c-6431ec0ff21e"
+        hierarchical = "e226ed04-e2b5-45b7-b03a-2257868e2553"
+        ransomware = "2fecf24e-6775-4c59-9c1f-3ed7a7d175a2"
+        apt = "d028e4fc-0b22-43b9-9c09-01fa21c1e145"
+    elif args.setting.lower() == "zeroscale":
+    # Zeroscale
+        ransomware = "b93772ab-cb80-480c-9273-3c8aefc28f11"
+        apt = "0c39e550-6f66-4b11-aea8-ac731391cb16"
+        hierarchical = "5127634f-c655-4d86-80b0-d21675a4638a"
+        multi_type = "c2b29221-8d55-42cc-b8bb-5c6a17cd6108"
+    elif args.setting.lower() == "zerotiny":
+    # Zerotiny
+        ransomware = "4a24954f-c835-40ee-b0fd-94d8a99bc52c"
+        apt = "9f2db5c8-bc83-4e3b-acb1-8127ca7db849"
+        hierarchical = "d8df4d88-7c36-4745-aa9a-9962a5bfa681"
+        multi_type = "2454ddd5-39fa-4fb7-a70f-a74c1c3bba85"
+    else:
+    # Improved 3.5k
+        multi_type = "0e867a54-df55-40b0-99fd-86b22e3f23a2"
+        hierarchical = "c2186cbc-a96d-4683-bf09-a565ac8743e3"
+        ransomware = "a94e9c2f-8a6a-45c5-9556-6a392b5401a7"
+        apt = "a19a9954-c220-42d6-a274-5567a19a31ad"
 
     mtrw_path = f"./saved_models/{multi_type}/ransomware.pth"
     mtapt_path = f"./saved_models/{multi_type}/apt.pth"
@@ -119,10 +151,11 @@ if __name__ == "__main__":
             args.append((copy.deepcopy(ENV), copy.deepcopy(red_agent), copy.deepcopy(blue_agent), copy.deepcopy(red_type), copy.deepcopy(name)))
     result_dict = dict()
 
-    with Pool(processes=8) as pool:
+    with Pool(processes=4) as pool:
         results = pool.starmap(evaluate, args)
         for name, red_rewards, blue_rewards in results:
             result_dict[f"{name}_red_rewards"] = red_rewards
             result_dict[f"{name}_blue_rewards"] = blue_rewards
-    with open("3k_results_test.json", "w") as f:
+            
+    with open(output_path, "w") as f:
         data = json.dump(result_dict, f)
--- src/run_tata_training.py
+++ src/run_tata_training.py
@@ -12,29 +12,10 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--type", type=str, help="What type of training to run", required=True)
-parser.add_argument("--steps", type=int, help="How many training steps to run", required=False)
+parser.add_argument("--game", type=str, help="Game ID", default="d2c92fc4-119d-42e8-83ab-45be03c64d49")
+parser.add_argument("--steps", type=int, help="How many training steps to run", default=3500)
 
 warnings.filterwarnings("ignore")
-
-TRIALS = 3500
-
-gdb = GameModeDB()
-ndb = NetworkDB()
-GAME_MODE = gdb.get("d2c92fc4-119d-42e8-83ab-45be03c64d49")
-NETWORK = ndb.get("3b921390-cd7b-41c5-8120-5e9ac587d2f2")
-
-network_interface = NetworkInterface(GAME_MODE, NETWORK)
-# Create dummy env to establish env parameters
-RED = AdaptiveRed(network_interface)
-BLUE = BlueInterface(network_interface)
-ENV = MultiAgentEnv(RED, BLUE, network_interface)
-
-blue_state_dim = ENV.observation_space.shape[0]
-blue_action_dim = ENV.action_space.n
-red_state_dim = ENV.observation_space.shape[0]
-red_action_dim = len(RED.action_dict)
-rw_path = "./saved_models/d2ff5e73-e9d3-48d6-a2c2-aa74bdd0aedc/blue.pth"
-apt_path = "./saved_models/98a681af-bf5f-430c-9e88-88b8b035a654/blue.pth"
 
 
 # Training function
@@ -55,6 +36,23 @@ def train(run, multi=False, hierarchical=False, red_agent_type=""):
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    gdb = GameModeDB()
+    ndb = NetworkDB()
+    GAME_MODE = gdb.get(args.game)
+    NETWORK = ndb.get("3b921390-cd7b-41c5-8120-5e9ac587d2f2")
+
+    network_interface = NetworkInterface(GAME_MODE, NETWORK)
+    # Create dummy env to establish env parameters
+    RED = AdaptiveRed(network_interface)
+    BLUE = BlueInterface(network_interface)
+    ENV = MultiAgentEnv(RED, BLUE, network_interface, print_metrics=False, print_per_ts_data=False)
+
+    blue_state_dim = ENV.observation_space.shape[0]
+    blue_action_dim = ENV.action_space.n
+    red_state_dim = ENV.observation_space.shape[0]
+    red_action_dim = len(RED.action_dict)
+    rw_path = "./saved_models/d2ff5e73-e9d3-48d6-a2c2-aa74bdd0aedc/blue.pth"
+    apt_path = "./saved_models/98a681af-bf5f-430c-9e88-88b8b035a654/blue.pth"
     red = AdaptiveRed
     blue = BlueInterface
     yt_run = MultiAgentYTRun(network=NETWORK,
@@ -62,7 +60,7 @@ if __name__ == "__main__":
                              red_agent_class=red,
                              blue_agent_class=blue,
                              auto=False,
-                             training_runs=TRIALS)
+                             training_runs=args.steps)
 
     if args.type == "multitype":
         train(yt_run, True, False)
